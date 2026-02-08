ba772efa743c38addc7f5e4c3e82f67ead512ca2
naitik-cmd <naitikchs16@gmail.com>
2026-02-09 02:30:29 +0530

    CRUD operations created. Database connected successfull.

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,12 @@
 //server start krna aur database se connect krna
+require('dotenv').config(); //to use variable created in .env file
+const mongoose= require("mongoose");
+const app= require('./src/app');
+const connectToDb= require('./src/config/database');
+
+connectToDb(); //DB connected
+
+
+app.listen(3000,()=>{
+    console.log("Server is running at port 3000.")
+})
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,59 @@
 //server create krna aur config krna
 const express= require("express");
 const app= express();
+const noteModel= require('./models/note.model');
+
+//middleware
+app.use(express.json());
+
+//create note API call
+app.post("/api/notes",async (req,res)=>{
+    const {title, description}= req.body;
+    const note= await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message: "Note created successfull!",
+        note
+    })
+
+})
+
+//fetch all note API call
+app.get("/api/notes",async (req,res)=>{
+
+    const notes= await noteModel.find();
+    
+    res.status(200).json({
+        message:"All notes fetched successfull",
+        notes
+    })
+})
+
+//delete note API call
+app.delete("/api/notes/:id",async (req,res)=>{
+    const id= req.params.id;
+
+    await noteModel.findByIdAndDelete(id);
+
+    res.status(200).json({
+        message:"Note deleted successfully",
+    })
+})
+
+//patch to update note
+app.patch('/api/notes/:id',async (req,res)=>{
+    const id = req.params.id
+    const {title, description}= req.body;
+
+    await noteModel.findByIdAndUpdate(id,{title, description});
+
+    res.status(200).json({
+        message:"Note Updation Successfull."
+    })
+})
+
+
 
 module.exports= app;
